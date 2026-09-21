54384b94b6d76ab861bde962f2e1b6478001c141
IanDeng1999 <ian.deng.x@gmail.com>
2026-09-21 15:44:28 +0800

    refactor: remove book demo module
    Co-authored-by: TRAE CLI <traecli@bytedance.com>

--- src/core/database/seeders/seeder.ts
+++ src/core/database/seeders/seeder.ts
@@ -1,9 +1,6 @@
 import type { EntityManager } from "@mikro-orm/core";
 import { Seeder } from "@mikro-orm/seeder";
-import { BookSeeder } from "./book.seeder.ts";
 
 export class DatabaseSeeder extends Seeder {
-  async run(em: EntityManager) {
-    await this.call(em, [BookSeeder]);
-  }
+  async run(_em: EntityManager) {}
 }
--- src/features/features.module.ts
+++ src/features/features.module.ts
@@ -1,7 +1,6 @@
 import { Module } from "@nestjs/common";
 import { AssetModule } from "./asset/asset.module.ts";
 import { AuthModule } from "./auth/auth.module.ts";
-import { BookModule } from "./book/book.module.ts";
 import { CronjobModule } from "./cronjob/cronjob.module.ts";
 import { EventsModule } from "./events/events.module.ts";
 import { HealthModule } from "./health/health.module.ts";
@@ -9,7 +8,6 @@ import { UserModule } from "./user/user.module.ts";
 
 @Module({
   imports: [
-    BookModule,
     CronjobModule,
     HealthModule,
     AuthModule,
