8d15e625f46c2a3f52a58bfa0c6d443c36299082
IanDeng1999 <ian.deng.x@gmail.com>
2026-09-22 11:13:55 +0800

    chore: note local dev APK hot reload config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,12 @@ const config: CapacitorConfig = {
   appId: "com.example.unnamedapp",
   appName: "Unnamed App",
   webDir: "dist",
+
+  // 本地开发 APK Hot Relaod
+  // server: {
+  //   url: "http://10.74.23.224:2121",
+  //   cleartext: true,
+  // },
 };
 
 export default config;
